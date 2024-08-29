ef6f11c182367d6f3985b989ebfb35220edec3d0
nc-hev-mag <150343854+nc-hev-mag@users.noreply.github.com>
2024-08-29 08:39:20 +0100

    reset listen

--- listen.js
+++ listen.js
@@ -1,11 +1,9 @@
 const app = require("./app");
 
-const { PORT = 9090 } = process.env;
-
-app.listen(PORT, (err) => {
+app.listen(9090, (err) => {
 	if (err) {
 		console.log(err);
 	} else {
-		console.log(`listening on port ${PORT}`);
+		console.log(`listening on port 9090`);
 	}
 });
